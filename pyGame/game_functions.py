ee6b3423f1ff9e02562d246b41908d018bafd911
dzyh <dzy0112@163.com>
2019-01-13 16:07:04 +0800

    添加子弹限制

--- pyGame/game_functions.py
+++ pyGame/game_functions.py
@@ -20,8 +20,9 @@ def check_keydown_events(event, ai_settings, screen, ship, bullets):
 		ship.moving_left = True
 	elif event.key == pygame.K_SPACE:
 		#创建一颗子弹，并将其加入到编组bullets中
-		new_bullet = Bullet(ai_settings, screen, ship)
-		bullets.add(new_bullet)
+		if len(bullets) < ai_settings.bullet_allowed:
+			new_bullet = Bullet(ai_settings, screen, ship)
+			bullets.add(new_bullet)
 
 def check_keyup_events(event, ship):
 	'''响应松开'''
