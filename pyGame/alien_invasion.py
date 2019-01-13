4f3f4e7faafe19d79db028a62a5eebcc298cfa23
dzyh <dzy0112@163.com>
2019-01-13 16:03:52 +0800

    删除已消失的子弹

--- pyGame/alien_invasion.py
+++ pyGame/alien_invasion.py
@@ -27,5 +27,9 @@ def run_game():
 		gf.check_events(ai_settings, screen, ship, bullets)
 		ship.update()
 		bullets.update()
+		#删除已消失的子弹
+		for bullet in bullets.copy():
+			if bullet.rect.bottom <=0:
+				bullets.remove(bullet)
 		gf.update_screen(ai_settings, screen, ship, bullets)
 run_game()
