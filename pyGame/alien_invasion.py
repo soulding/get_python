52961d3d61441b2c57b37f9b98a5ef33d7138bf1
dzyh <dzy0112@163.com>
2019-01-23 22:15:22 +0800

    添加点击Play按钮事件

--- pyGame/alien_invasion.py
+++ pyGame/alien_invasion.py
@@ -34,7 +34,7 @@ def run_game():
 	gf.create_fleet(ai_settings, screen, ship, aliens)
 	#开始游戏主循环
 	while True:
-		gf.check_events(ai_settings, screen, ship, bullets)
+		gf.check_events(ai_settings, screen, stats, play_button, ship, bullets)
 		if stats.game_active:
 			ship.update()
 			gf.update_bullets(ai_settings, screen, ship, aliens, bullets)
--- pyGame/game_functions.py
+++ pyGame/game_functions.py
@@ -4,7 +4,7 @@ import pygame
 from bullet import Bullet
 from alien import Alien
 
-def check_events(ai_settings, screen, ship, bullets):
+def check_events(ai_settings, screen, stats, play_button, ship, bullets):
 	'''相应按键和鼠标事件'''
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
@@ -13,7 +13,15 @@ def check_events(ai_settings, screen, ship, bullets):
 			check_keydown_events(event, ai_settings, screen, ship, bullets)
 		elif event.type == pygame.KEYUP:
 			check_keyup_events(event,  ship)
-				
+		elif event.type == pygame.MOUSEBUTTONDOWN:
+			mouse_x, mouse_y = pygame.mouse.get_pos()
+			check_play_button(stats, play_button, mouse_x, mouse_y)
+		
+def check_play_button(stats, play_button, mouse_x, mouse_y):
+	"""在玩家胆机Play按钮时开始新游戏"""
+	if play_button.rect.collidepoint(mouse_x, mouse_y):
+		stats.game_active = True
+
 def check_keydown_events(event, ai_settings, screen, ship, bullets):
 	'''响应按键'''
 	if event.key == pygame.K_RIGHT:
